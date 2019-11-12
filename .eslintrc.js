a7f6da2f3676b53b6c4682d05a69d639ed213677
Dev-Nebe <cp.nebe@gmail.com>
2019-11-12 15:39:15 +0100

    Setup linting, templating engine and routing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,18 @@
 module.exports = {
-	'env': {
-		'commonjs': true,
-		'es6': true,
-		'node': true
-	},
-	'extends': 'eslint:recommended',
-	'globals': {
-		'Atomics': 'readonly',
-		'SharedArrayBuffer': 'readonly'
-	},
-	'parserOptions': {
-		'ecmaVersion': 2018
-	},
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
-	}
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "airbnb-base",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "comma-dangle": 0
+    }
 };
--- app.js
+++ app.js
@@ -1,23 +1,52 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable indent */
+
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-const app = express();
 
+
+const app = express();
+const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
+const books = [];
+// Next hard code the books and include them.
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist')));
+
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+
+bookRouter.route('/')
+.get((req, res) => {
+  res.render('books', {
+    nav: [{ link: '/books', title: 'Books' },
+    { link: '/authors', title: 'Authors' }],
+    title: 'Library App'
+});
+});
+app.use('/books', bookRouter);
 
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname,'views/index.html'));
+  // res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render('index.ejs', {
+    nav: [{ link: '/books', title: 'Books' },
+    { link: '/authors', title: 'Authors' }],
+    title: 'Library App'
+});
 });
 
-app.listen(3000, () => {
-    debug(`listening on port ${chalk.green('3000')}`);
+
+app.listen(port, () => {
+    debug(`listening on port ${chalk.green(`${port}`)}`);
 });
