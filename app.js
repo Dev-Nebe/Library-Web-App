bcafcae00f97370ae6a346fc9da30edb3049ac92
Dev-Nebe <cp.nebe@gmail.com>
2019-11-14 02:49:27 +0100

    Setup SQL

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const sql = require('mssql');
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const config = {
+  
+};
+
+sql.connect(config).catch((err) => debug(err));
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,11 +1,14 @@
 /* eslint-disable linebreak-style */
 
 const express = require('express');
+const sql = require('mssql');
+const debug = require('debug')('app:bookRoutes');
+
 
 const bookRouter = express.Router();
 
 const router = (nav) => {
-  const books = [
+  let books = [
     {
       title: 'The Wind in the Willows',
       genre: 'Fantasy',
@@ -28,22 +31,31 @@ const router = (nav) => {
 
   bookRouter.route('/')
     .get((req, res) => {
-      res.render('bookListView',
-        {
-          nav,
-          title: 'Books',
-          books
-        });
+      (async function query() {
+        const request = new sql.Request();
+        const { recordset } = await request.query('select * from books');
+        books = recordset;
+        res.render('bookListView',
+          {
+            nav,
+            title: 'Books',
+            books
+          });
+      }());
     });
 
   bookRouter.route('/:id').get((req, res) => {
-    const { id } = req.params;
-    res.render('bookView',
-      {
-        nav,
-        title: 'Book',
-        book: books[id]
-      });
+    (async function query() {
+      const { id } = req.params;
+      const request = new sql.Request();
+      const { recordset } = await request.input('id', sql.Int, id).query('select * from books where id = @id');
+      res.render('bookView',
+        {
+          nav,
+          title: 'Book',
+          book: recordset[0]
+        });
+    }());
   });
 
   return bookRouter;
